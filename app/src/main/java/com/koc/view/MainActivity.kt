7ffe666b2a796d0402033fb05fb1d7563b94428c
Owusu Kelvin Clark <kelvinclark2k77@gamil.com>
2021-07-15 00:23:10 +0100

    :construction_worker: Use custom toolbar

--- app/src/main/java/com/koc/view/MainActivity.kt
+++ app/src/main/java/com/koc/view/MainActivity.kt
@@ -1,16 +1,30 @@
 package com.koc.view
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.widget.Toolbar
+import androidx.navigation.findNavController
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.setupWithNavController
 import com.koc.blogg.R
 import com.koc.blogg.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
-    lateinit var binding: ActivityMainBinding
+    private lateinit var binding: ActivityMainBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+
+        val toolbar: Toolbar = binding.toolbar
+
+        val navHost = supportFragmentManager.findFragmentById(R.id.fragment_host) as NavHostFragment
+        val navController = navHost.navController
+        val appBarConfig = AppBarConfiguration(navController.graph)
+        toolbar.setupWithNavController(navController, appBarConfig)
     }
+
 }
