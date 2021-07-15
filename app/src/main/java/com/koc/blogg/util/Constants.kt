53aa2ce4c3fda57be7299ab2b4434b8876a4193e
Owusu Kelvin Clark <kelvinclark2k77@gamil.com>
2021-07-15 03:32:09 +0100

    :construction_worker: Fix api url
    I mistakenly used localhost thinking it's the localhost
    of my machine. Am now using the universal 10.0.2.2
    address to make connection to the API.

--- app/src/main/java/com/koc/blogg/util/Constants.kt
+++ app/src/main/java/com/koc/blogg/util/Constants.kt
@@ -4,5 +4,5 @@ package com.koc.blogg.util
 Created by kelvin_clark on 7/15/2021 2:39 AM
  */
 object Constants {
-    const val BASE_URL = "http://localhost:8000/api/v0.1.0/"
+    const val BASE_URL = "http://10.0.2.2:8000/api/v0.1.0/"
 }
