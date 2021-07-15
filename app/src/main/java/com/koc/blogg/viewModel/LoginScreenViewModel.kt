5d78d1c9e620f594844371f76541214a1ea304c5
Owusu Kelvin Clark <kelvinclark2k77@gamil.com>
2021-07-15 03:02:32 +0100

    :construction_worker: Handle User Login
    Created a login function in the view model for treating
    cases of both success and failure. In either of the cases
    an event will be sent to the Fragment to notify of success
    or failure.

--- app/src/main/java/com/koc/blogg/viewModel/LoginScreenViewModel.kt
+++ app/src/main/java/com/koc/blogg/viewModel/LoginScreenViewModel.kt
@@ -1,16 +1,39 @@
 package com.koc.blogg.viewModel
 
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.koc.blogg.repository.BloggRepository
+import com.koc.blogg.util.LoginEvent
+import com.koc.blogg.util.ResponseState
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.Dispatchers.IO
+import kotlinx.coroutines.channels.Channel
+import kotlinx.coroutines.flow.receiveAsFlow
+import kotlinx.coroutines.launch
 import javax.inject.Inject
 
 /**
 Created by kelvin_clark on 7/15/2021 1:28 AM
  */
 @HiltViewModel
-class LoginScreenViewModel @Inject constructor(): ViewModel() {
+class LoginScreenViewModel @Inject constructor(
+    private val repository: BloggRepository
+) : ViewModel() {
 
     var email = ""
     var password = ""
 
+    private val loginEventChannel = Channel<LoginEvent>()
+    val loginEvent = loginEventChannel.receiveAsFlow()
+
+    fun loginUser() = viewModelScope.launch(IO) {
+        when (val result = repository.loginUser(email=email, password=password)) {
+            is ResponseState.Success -> {
+                loginEventChannel.send(LoginEvent.LoginSuccessFull(userId = result.data!!.id))
+            }
+            is ResponseState.Error -> {
+                loginEventChannel.send(LoginEvent.ErrorLogin)
+            }
+        }
+    }
 }
