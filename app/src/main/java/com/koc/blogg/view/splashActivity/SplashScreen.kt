ff023dad6964468c7651e0e8353a8d3b3b1d5449
Owusu Kelvin Clark <kelvinclark2k77@gamil.com>
2021-07-20 14:18:48 +0100

    :building_construction: Navigate to blog activity when authenticated

--- app/src/main/java/com/koc/blogg/view/splashActivity/SplashScreen.kt
+++ app/src/main/java/com/koc/blogg/view/splashActivity/SplashScreen.kt
@@ -7,6 +7,7 @@ import androidx.lifecycle.lifecycleScope
 import com.koc.blogg.databinding.SplashScreenBinding
 import com.koc.blogg.util.PreferenceManager
 import com.koc.blogg.view.authenticationActivity.AuthenticationActivity
+import com.koc.blogg.view.blogActivity.BlogActivity
 import dagger.hilt.android.AndroidEntryPoint
 import kotlinx.coroutines.Dispatchers.IO
 import kotlinx.coroutines.flow.first
@@ -33,11 +34,13 @@ class SplashScreen: AppCompatActivity() {
 
     private fun checkIfUserIsRegistered() = lifecycleScope.launch(IO) {
         preferenceManager.userId.first {userId ->
-            if (userId == null) {
-                val intent = Intent(this@SplashScreen, AuthenticationActivity::class.java)
-                intent.flags = Intent.FLAG_ACTIVITY_CLEAR_TASK or Intent.FLAG_ACTIVITY_NEW_TASK
-                startActivity(intent)
+            val intent = if (userId == null) {
+                Intent(this@SplashScreen, AuthenticationActivity::class.java)
+            }else {
+                Intent(this@SplashScreen, BlogActivity::class.java)
             }
+            intent.flags = Intent.FLAG_ACTIVITY_CLEAR_TASK or Intent.FLAG_ACTIVITY_NEW_TASK
+            startActivity(intent)
             true
         }
     }
