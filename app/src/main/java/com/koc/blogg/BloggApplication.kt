ba09c26ef672081193372abc9d8b133c8e1aec84
Owusu Kelvin Clark <kelvinclark2k77@gamil.com>
2021-07-20 16:08:11 +0100

    :construction: Create BlogList View model

--- app/src/main/java/com/koc/blogg/BloggApplication.kt
+++ app/src/main/java/com/koc/blogg/BloggApplication.kt
@@ -1,21 +1,10 @@
 package com.koc.blogg
 
 import android.app.Application
-import android.os.StrictMode
 import dagger.hilt.android.HiltAndroidApp
 
 /**
 Created by kelvin_clark on 7/15/2021 1:25 AM
  */
 @HiltAndroidApp
-class BloggApplication: Application() {
-    override fun onCreate() {
-        super.onCreate()
-        if(BuildConfig.DEBUG) {
-            StrictMode.setVmPolicy(StrictMode.VmPolicy.Builder()
-                .detectAll()
-                .penaltyLog()
-                .build())
-        }
-    }
-}
+class BloggApplication: Application()
--- app/src/main/java/com/koc/blogg/view/blogActivity/blogListScreen/BlogListFragment.kt
+++ app/src/main/java/com/koc/blogg/view/blogActivity/blogListScreen/BlogListFragment.kt
@@ -5,10 +5,12 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.viewModels
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.koc.blogg.databinding.BlogListScreenBinding
 import com.koc.blogg.util.BlogItemClickedListener
 import com.koc.blogg.view.blogActivity.blogListScreen.adapters.BlogListAdapter
+import com.koc.blogg.viewModel.BlogListScreenViewModel
 import dagger.hilt.android.AndroidEntryPoint
 
 /**
@@ -18,6 +20,7 @@ Created by kelvin_clark on 7/20/2021 2:01 PM
 class BlogListFragment: Fragment(), BlogItemClickedListener {
 
     private val listAdapter = BlogListAdapter(this)
+    private val viewModel: BlogListScreenViewModel by viewModels()
 
     private var _binding: BlogListScreenBinding? = null
     private val binding
@@ -40,10 +43,20 @@ class BlogListFragment: Fragment(), BlogItemClickedListener {
                 layoutManager = LinearLayoutManager(requireContext())
             }
         }
+        observeBlogList()
+    }
+
+    private fun observeBlogList() {
+        viewModel.blogList.observe(viewLifecycleOwner) { blogList ->
+            if (blogList.isNotEmpty()) {
+                listAdapter.submitList(blogList)
+            }
+        }
     }
 
     override fun onDestroy() {
         super.onDestroy()
+        listAdapter.clearBinding()
         _binding = null
     }
 
