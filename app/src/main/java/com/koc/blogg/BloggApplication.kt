968774c15e4cb864a908127b688b59e68c7b34f1
Owusu Kelvin Clark <kelvinclark2k77@gamil.com>
2021-07-20 15:54:29 +0100

    :heavy_plus_sign: Add leak canary dependency
    There is a resource leaking memory and am finding it difficult to
    find what is causing it. I've used Strict mode but not much of an
    info is given. So am trying leak canary.

--- app/src/main/java/com/koc/blogg/BloggApplication.kt
+++ app/src/main/java/com/koc/blogg/BloggApplication.kt
@@ -1,10 +1,21 @@
 package com.koc.blogg
 
 import android.app.Application
+import android.os.StrictMode
 import dagger.hilt.android.HiltAndroidApp
 
 /**
 Created by kelvin_clark on 7/15/2021 1:25 AM
  */
 @HiltAndroidApp
-class BloggApplication: Application()
+class BloggApplication: Application() {
+    override fun onCreate() {
+        super.onCreate()
+        if(BuildConfig.DEBUG) {
+            StrictMode.setVmPolicy(StrictMode.VmPolicy.Builder()
+                .detectAll()
+                .penaltyLog()
+                .build())
+        }
+    }
+}
